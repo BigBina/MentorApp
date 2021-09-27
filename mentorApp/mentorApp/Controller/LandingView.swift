058f0577aed8985c830f8f75bb24c70a20e0888a
Brandon Brown <brandonjsu@gmail.com>
2021-09-27 07:45:55 -0500

    adding gitignore

---
+++ mentorApp/mentorApp/Controller/LandingView.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mentorApp
+//
+//  Created by Brandon Brown on 9/26/21.
+//
+
+import UIKit
+
+class LandingView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
