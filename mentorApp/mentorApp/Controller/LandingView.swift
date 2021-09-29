ce58d371316f7e19c464b7422cb7db59553f16c9
Brandon Brown <brandonjsu@gmail.com>
2021-09-28 21:05:11 -0500

    Adding the Apple Credentials struct and the Auth credentials for SIWA. LandingView not clean

--- mentorApp/mentorApp/Controller/LandingView.swift
+++ mentorApp/mentorApp/Controller/LandingView.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import AuthenticationServices
+import Firebase
+import CryptoKit
 
 class LandingView: UIViewController {
 
@@ -21,6 +23,11 @@ class LandingView: UIViewController {
         buttonProperties()
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        sign.frame = CGRect(x: 0, y: 0, width: 250, height: 50)
+    }
+    
     func buttonProperties(){
         
         view.addSubview(sign)
@@ -32,21 +39,30 @@ class LandingView: UIViewController {
         
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        sign.frame = CGRect(x: 0, y: 0, width: 250, height: 50)
-        
-    }
-    
+
+    fileprivate var currentNonce: String?
     @objc func didTapSignIn(){
+        let nonce = randomNonceString()
+        currentNonce = nonce
         let provider = ASAuthorizationAppleIDProvider()
         let request = provider.createRequest()
         request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
         
         let controller = ASAuthorizationController(authorizationRequests: [request])
         controller.delegate = self
         controller.presentationContextProvider = self
         controller.performRequests()
+    }
+    
+    private func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        return String(format: "%02x", $0)
+      }.joined()
+        return hashString
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -59,31 +75,86 @@ class LandingView: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 
-    
-
 }
-
+//MARK: - Apple Authorization
 extension LandingView: ASAuthorizationControllerDelegate {
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
         print("Woah")
     }
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
         
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+//            UserDefaults.standard.set(appleIDCredential.user, forKey: "appleAuthorizedUserIdKey")
+          guard let nonce = currentNonce else {
+            fatalError("Invalid state: A login callback was received, but no login request was sent.")
+          }
+          guard let appleIDToken = appleIDCredential.identityToken else {
+            print("Unable to fetch identity token")
+            return
+          }
+          guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+            print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+            return
+          }
+          // Initialize a Firebase credential.
+            let firebaseCredential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                              idToken: idTokenString,
+                                                              rawNonce: nonce)
+          // Sign in with Firebase.
+          Auth.auth().signIn(with: firebaseCredential) { (authResult, error) in
+            if let e = error {
+              print(e.localizedDescription)
+            }
+          }
+        }
         switch authorization.credential {
         case let appleIDCredential as ASAuthorizationAppleIDCredential:
-            
-            // Create an account in your system.
-            let userIdentifier = appleIDCredential.user
-//            let fullName = appleIDCredential.fullName
-            let firstName = appleIDCredential.fullName?.givenName
-            let lastName = appleIDCredential.fullName?.familyName
-            let email = appleIDCredential.email
-            break
+
+
+            //Create an account in your system.
+            let user = Apple(appleIDCredentials: appleIDCredential)
+            if user.email != ""{
+                self.performSegue(withIdentifier: "AppleToRegister", sender: self)
+            } else {
+                self.performSegue(withIdentifier: "AppleToMain", sender: self)
+            }
         
+            break
         default:
             break
         }
     
+    }
+    private func randomNonceString(length: Int = 32) -> String {
+      precondition(length > 0)
+      let charset: Array<Character> =
+          Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+      var result = ""
+      var remainingLength = length
+
+      while remainingLength > 0 {
+        let randoms: [UInt8] = (0 ..< 16).map { _ in
+          var random: UInt8 = 0
+          let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+          if errorCode != errSecSuccess {
+            fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+          }
+          return random
+        }
+
+        randoms.forEach { random in
+          if remainingLength == 0 {
+            return
+          }
+
+          if random < charset.count {
+            result.append(charset[Int(random)])
+            remainingLength -= 1
+          }
+        }
+      }
+
+      return result
     }
 }
 
@@ -92,5 +163,5 @@ extension LandingView: ASAuthorizationControllerPresentationContextProviding {
         return view.window!
     }
     
-    
 }
+
--- mentorApp/mentorApp/Model/AppleCredentials.swift
+++ mentorApp/mentorApp/Model/AppleCredentials.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import AuthenticationServices
+
+struct Apple{
+    let user : String
+    let firstName : String
+    let lastName : String
+    let email : String
+    
+    init(appleIDCredentials: ASAuthorizationAppleIDCredential){
+        self.user = appleIDCredentials.user
+        self.firstName = appleIDCredentials.fullName?.givenName ?? ""
+        self.lastName = appleIDCredentials.fullName?.familyName ?? ""
+        self.email = appleIDCredentials.email ?? ""
+    }
+}
+
