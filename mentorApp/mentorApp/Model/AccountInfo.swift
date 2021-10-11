767cb278a5334f9e9bd65ab8fa8cf1ae698bbf83
Brandon Brown <brandonjsu@gmail.com>
2021-10-11 18:50:07 -0500

    Updated the profile struct

--- mentorApp/mentorApp/Model/AccountInfo.swift
+++ mentorApp/mentorApp/Model/AccountInfo.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct Profile {
+    var firstName : String
+    let lastName : String
+    var phoneNumber : String
+    let city : String
+    let state : String
+    let bio : String
+    // Mentor or mentee
+    let type : String
+    // All selected will from categories page will be here
+    var categories : [String]
+//    var interest : [String]
+    // This is seperate. Only choose 1 from the picker view.
+    var career : String
+    
+}
+
+
