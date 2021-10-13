472776a118560511358037aa691c79f527fd8819
Brandon Brown <brandonjsu@gmail.com>
2021-10-13 02:50:05 -0500

    Added init function, might not need it. Also wrapped the properties in Profile as optionals

--- mentorApp/mentorApp/Model/AccountInfo.swift
+++ mentorApp/mentorApp/Model/AccountInfo.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-struct Profile {
-    var firstName : String
-    let lastName : String
-    var phoneNumber : String
-    let city : String
-    let state : String
+public struct Profile {
+    var firstName : String?
+    var lastName : String?
+    var phoneNumber : String?
+    var city : String?
+    var state : String?
     let bio : String
     // Mentor or mentee
     let type : String
@@ -22,6 +22,18 @@ struct Profile {
     // This is seperate. Only choose 1 from the picker view.
     var career : String
     
+    init(firstName: String, lastName: String, phoneNumber: String, city: String, state: String, bio: String, type: String, career: String ,categories: [String]) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.career = career
+        self.phoneNumber = phoneNumber
+        self.city = city
+        self.state = state
+        self.type = type
+        self.categories = categories
+        self.career = career
+        self.bio = bio
+    }
 }
 
 
