859560af3cc55c9ec58e96e1ab3b854808edf3cd
Brandon Brown <brandonjsu@gmail.com>
2021-11-05 14:03:34 -0500

    Added the accept action that updates the dataDoc with connections that are accepted. Updated the queryMethod with initialQuery. (Originially named this because the query on load would not accept a user since the OG function just called the query method) In the update: 	- Grabbed the email from the query and stored it inside the Mentor struct. 	- Removed query from the local method 	- Stored the count (Not indexed yet)

--- mentorApp/mentorApp/Controller/HomepageView.swift
+++ mentorApp/mentorApp/Controller/HomepageView.swift
@@ -22,31 +22,33 @@ class HomepageView: UIViewController {
     @IBOutlet weak var denyButton: UIButton!
     @IBOutlet weak var acceptButton: UIButton!
     
-    
+    let defaults = UserDefaults.standard
     
 
     var docArrayStart = 0
+    var num = 0
+    var mentor : MentorProfile = MentorProfile(firstName: nil, lastName: nil, email: "nil", city: nil, state: nil, bio: "nil", categories: ["nil"], career: "nil")
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        queryMethod()
+//        queryMethod()
+        initalQuery()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        
-    }
+    
+    
+    
     var user = Firebase.Auth.auth().currentUser
+    let query = Global.db.collection("userData").whereField("type", isEqualTo: "Mentor")
 
-
-    func queryMethod(){
-        
-        let typeRef = Global.db.collection("userData")
-        let query = typeRef.whereField("type", isEqualTo: "Mentor")
-        
-        query.getDocuments { querySnapshot, err in
-          
+    func initalQuery(){
+        query.getDocuments() { querySnapshot, err in
+            
+            self.num += 1
+            print("Count of runtime \(self.num)")
             if let err = err {
                 print("Error getting documents: \(err)")
           } else {
@@ -56,12 +58,13 @@ class HomepageView: UIViewController {
                 let alert = UIAlertController(title: "Error", message: "Ran out of Users", preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
                 self.present(alert, animated: true, completion: nil)
-                self.acceptButton.isHidden = true
-                self.denyButton.isHidden = true
+
                 return
             }
-                if let document = querySnapshot?.documents[self.docArrayStart]{//0 - 1 -2
-                    let data = document.data()
+            //MARK: - Populate the page with Mentor Data
+            if let document = querySnapshot?.documents[self.docArrayStart]{//0 - 1 -2
+                let data = document.data()
+//                let mentorID = Firebase.Auth.auth().currentUser?.email
                     if let firstN = data["firstName"] as? String,
                        let lastN = data["lastName"] as? String,
                        let city = data["city"] as? String,
@@ -69,7 +72,9 @@ class HomepageView: UIViewController {
                        let bio = data["bio"] as? String,
                        let categories = data["categories"] as? [String],
                        let career = data["career"] as? String,
-                       let pic = data["profile-image"] as? String
+                       let pic = data["profile-image"] as? String,
+                       let email = data["email"] as? String
+                      
                     {
                         let storageRef = Storage.storage().reference(forURL: pic)
                         self.profilePicture.sd_setImage(with: storageRef, placeholderImage: UIImage(named: "\(Global.userID!).png"))
@@ -81,23 +86,47 @@ class HomepageView: UIViewController {
                         for category in categories {
                             self.listOfCategories.text! += "\(category)\n"
                         }
+                        
+                        
+                        Global.db.collection("userData").document(Global.userID!).updateData([
+                            "count" : self.docArrayStart
+                        ]) { err in
+                            if let err = err {
+                                print("Error updating document: \(err)")
+                            } else {
+                                print("Document successfully updated")
+                            }
+                        }
+                        //MARK: -
+                        self.mentor.email = email
+                        
                      }
-                    print("\(document.documentID) => \(document.data())")
                 }
                 self.docArrayStart += 1 //1 - 2 - 3
             }
         }
     }
+
     
     @IBAction func denyAction(_ sender: Any) {
-        queryMethod()
-        self.listOfCategories.text = ""
+        initalQuery()
     }
     
     @IBAction func acceptAction(_ sender: Any) {
-        queryMethod()
-        self.listOfCategories.text = ""
-
+        Global.db.collection("userData").document(Global.userID!).updateData([
+            "connections" : FieldValue.arrayUnion([self.mentor.email])
+        ]){ err in
+            if let err = err {
+                print("Error updating document: \(err)")
+            } else {
+                print("Document successfully updated")
+            }
+        }
+        DispatchQueue.main.async{
+            self.listOfCategories.text = ""
+        }
+        initalQuery()
     }
+    
     
 }
