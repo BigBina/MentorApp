a88f143aaaa5fe8bd19092403ebcc6d45916896c
Brandon Brown <brandonjsu@gmail.com>
2021-11-05 14:55:05 -0500

    Adding the mentorProfile struct to carry my docData to the cells/messages

--- mentorApp/mentorApp/Model/MentorProflie.swift
+++ mentorApp/mentorApp/Model/MentorProflie.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct MentorProfile {
+    var firstName : String?
+    var lastName : String?
+    var email : String
+    var city : String?
+    var state : String?
+    var bio : String
+    // Mentor or mentee
+    // All selected will from categories page will be here
+    var categories : [String]
+//    var interest : [String]
+    // This is seperate. Only choose 1 from the picker view.
+    var career : String
+}
