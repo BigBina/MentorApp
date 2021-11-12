1a1c97c61c3ff7cc7fc0f895cf5036e1356fa452
Brandon Brown <brandonjsu@gmail.com>
2021-11-12 11:42:13 -0600

    Added the profile to a global function.

--- mentorApp/mentorApp/Model/Global.swift
+++ mentorApp/mentorApp/Model/Global.swift
@@ -11,4 +11,8 @@ import Firebase
 class Global: NSObject {
     static let db = Firestore.firestore()
     static let userID = Auth.auth().currentUser?.uid
+    static let userEmail = Auth.auth().currentUser?.email
+    static var displayName = Auth.auth().currentUser?.displayName
+    
+    static var profile : Profile = Profile(firstName: "", lastName: "", phoneNumber: "", city: "", state: "", bio: "", type: "", career: "", categories: [""])
 }
