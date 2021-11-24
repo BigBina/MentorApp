22acfd835d22b20c2809410ed933ef94ea4511d7
Brandon Brown <brandonjsu@gmail.com>
2021-11-24 17:18:54 -0600

    Added the Conversation and LatestMessage struct for Firebase.

--- mentorApp/mentorApp/Model/Message.swift
+++ mentorApp/mentorApp/Model/Message.swift
@@ -52,3 +52,16 @@ extension MessageKind{
         }
     }
 }
+
+struct Conversation {
+    let id: String
+    let name: String
+    let otherUserEmail : String
+    let latestMessage: LatestMessage
+}
+
+struct LatestMessage{
+    let date: String
+    let text: String
+    let isRead: Bool
+}
